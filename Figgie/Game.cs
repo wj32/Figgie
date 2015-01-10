6d2271f036c2edeea20ac1c2719cc67db4b09762
Wen Jia Liu <wj32.64@gmail.com>
2015-01-10 18:15:14 -0500

    Initial files

---
+++ Figgie/Game.cs
@@ -0,0 +1,426 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Figgie
+{
+    public enum Suit : int
+    {
+        Hearts = 0,
+        Diamonds,
+        Clubs,
+        Spades
+    }
+
+    public class Suits
+    {
+        public static readonly IReadOnlyCollection<Suit> All =
+            new Suit[] { Suit.Hearts, Suit.Diamonds, Suit.Clubs, Suit.Spades };
+
+        public static Suit Opposite(Suit suit)
+        {
+            switch (suit)
+            {
+                case Suit.Hearts:
+                    return Suit.Diamonds;
+                case Suit.Diamonds:
+                    return Suit.Hearts;
+                case Suit.Clubs:
+                    return Suit.Spades;
+                case Suit.Spades:
+                    return Suit.Clubs;
+                default:
+                    throw new ArgumentException("suit");
+            }
+        }
+    }
+
+    public interface IReadOnlyHand
+    {
+        int Count(Suit suit);
+    }
+
+    public class Hand : IReadOnlyHand
+    {
+        private int[] _counts = new int[Suits.All.Count];
+
+        public Hand()
+        { }
+
+        public Hand(IEnumerable<Suit> cards)
+        {
+            foreach (var card in cards)
+                _counts[(int)card]++;
+        }
+
+        private Hand(int[] counts)
+        {
+            Array.Copy(counts, _counts, _counts.Length);
+        }
+
+        public int Count(Suit suit)
+        {
+            return _counts[(int)suit];
+        }
+
+        public void Add(Suit suit, int n = 1)
+        {
+            _counts[(int)suit] += n;
+        }
+
+        public void Remove(Suit suit, int n = 1)
+        {
+            _counts[(int)suit] -= n;
+        }
+
+        public Hand Clone()
+        {
+            return new Hand(_counts);
+        }
+    }
+
+    public interface IPlayer
+    {
+        void Initialize(IPlayerGame game);
+    }
+
+    public interface IPlayerGame
+    {
+        int NumberOfPlayers { get; }
+        int PlayerId { get; }
+        IReadOnlyHand Hand { get; }
+        IPlayerMarket Market(Suit suit);
+    }
+
+    public interface IPlayerMarket
+    {
+        int? BestBid { get; }
+        int? BestAsk { get; }
+        bool Bid(int price);
+        bool Ask(int price);
+        void Out();
+
+        bool Buy();
+        bool Sell();
+    }
+
+    public class Game
+    {
+        public class Player
+        {
+            public int Cash;
+            public Hand Hand = new Hand();
+
+            public IPlayer ExternalPlayer;
+            public IPlayerGame PlayerGame;
+        }
+
+        private class Market
+        {
+            public Game Game;
+            public Suit Suit;
+            public Tuple<int, int> BestBid;
+            public Tuple<int, int> BestAsk;
+
+            public bool Bid(int price, int playerId)
+            {
+                if (Game._ended)
+                    return false;
+
+                if (BestBid == null || price > BestBid.Item1)
+                {
+                    BestBid = new Tuple<int, int>(price, playerId);
+                    return true;
+                }
+
+                return false;
+            }
+
+            public bool Ask(int price, int playerId)
+            {
+                if (Game._ended)
+                    return false;
+                // No short selling allowed.
+                if (Game._players[playerId].Hand.Count(Suit) <= 0)
+                    return false;
+
+                if (BestAsk == null || price < BestAsk.Item1)
+                {
+                    BestAsk = new Tuple<int, int>(price, playerId);
+                    return true;
+                }
+
+                return false;
+            }
+
+            public void Out(int playerId)
+            {
+                if (Game._ended)
+                    return;
+                if (BestBid != null && BestBid.Item2 == playerId)
+                    BestBid = null;
+                if (BestAsk != null && BestAsk.Item2 == playerId)
+                    BestAsk = null;
+            }
+
+            private void Settle(int buyerId, int sellerId, int price)
+            {
+                Game._players[buyerId].Cash -= price;
+                Game._players[sellerId].Cash += price;
+                Game._players[buyerId].Hand.Add(Suit);
+                Game._players[sellerId].Hand.Remove(Suit);
+            }
+
+            public bool Buy(int buyerId)
+            {
+                if (Game._ended || BestAsk == null)
+                    return false;
+
+                int price = BestAsk.Item1;
+                int sellerId = BestAsk.Item2;
+
+                if (sellerId == buyerId)
+                    return false;
+
+                Settle(buyerId, sellerId, price);
+                BestAsk = null;
+
+                return true;
+            }
+
+            public bool Sell(int sellerId)
+            {
+                if (Game._ended || BestBid == null)
+                    return false;
+                // No short selling allowed.
+                if (Game._players[sellerId].Hand.Count(Suit) <= 0)
+                    return false;
+
+                int price = BestBid.Item1;
+                int buyerId = BestBid.Item2;
+
+                if (buyerId == sellerId)
+                    return false;
+
+                Settle(buyerId, sellerId, price);
+                BestBid = null;
+
+                return true;
+            }
+        }
+
+        private class PlayerGame : IPlayerGame
+        {
+            private Game _game;
+            private int _playerId;
+            private PlayerMarket[] _markets;
+
+            public PlayerGame(Game game, int playerId)
+            {
+                _game = game;
+                _playerId = playerId;
+                _markets = new PlayerMarket[Suits.All.Count];
+
+                foreach (var suit in Suits.All)
+                    _markets[(int)suit] = new PlayerMarket(game, playerId, suit);
+            }
+
+            public int NumberOfPlayers
+            {
+                get { return _game._players.Length; }
+            }
+
+            public int PlayerId
+            {
+                get { return _playerId; }
+            }
+
+            public IReadOnlyHand Hand
+            {
+                get { return _game._players[_playerId].Hand; }
+            }
+
+            public IPlayerMarket Market(Suit suit)
+            {
+                return _markets[(int)suit];
+            }
+        }
+
+        private class PlayerMarket : IPlayerMarket
+        {
+            private Game _game;
+            private int _playerId;
+            private Suit _suit;
+            private Market _market;
+
+            public PlayerMarket(Game game, int playerId, Suit suit)
+            {
+                _game = game;
+                _playerId = playerId;
+                _suit = suit;
+                _market = _game._markets[(int)_suit];
+            }
+
+            public int? BestBid
+            {
+                get
+                {
+                    var best = _market.BestBid;
+                    if (best != null)
+                        return best.Item1;
+                    else
+                        return null;
+                }
+            }
+
+            public int? BestAsk
+            {
+                get
+                {
+                    var best = _market.BestAsk;
+                    if (best != null)
+                        return best.Item1;
+                    else
+                        return null;
+                }
+            }
+
+            public bool Bid(int price)
+            {
+                return _market.Bid(price, _playerId);
+            }
+
+            public bool Ask(int price)
+            {
+                return _market.Ask(price, _playerId);
+            }
+
+            public void Out()
+            {
+                _market.Out(_playerId);
+            }
+
+            public bool Buy()
+            {
+                return _market.Buy(_playerId);
+            }
+
+            public bool Sell()
+            {
+                return _market.Sell(_playerId);
+            }
+        }
+
+        private Random _random;
+        private Player[] _players;
+        private Market[] _markets;
+        private Suit _goalSuit;
+        private bool _ended;
+
+        public Game(IPlayer[] players)
+            : this(players, (new Random((int)DateTime.Now.ToFileTime())).Next())
+        { }
+
+        public Game(IPlayer[] players, int seed)
+        {
+            _random = new Random(seed);
+
+            if (players.Length != 4 && players.Length != 5)
+                throw new ArgumentException("Number of players must be 4 or 5.");
+
+            int buyIn = 200 / players.Length;
+            int handSize = 40 / players.Length;
+
+            var suitSizes = new List<int> { 8, 10, 10, 12 };
+            Shuffle(suitSizes);
+            _goalSuit = Suits.Opposite((Suit)suitSizes.IndexOf(12));
+            var cards =
+                Enumerable.Repeat(Suit.Hearts, suitSizes[(int)Suit.Hearts])
+                .Concat(Enumerable.Repeat(Suit.Diamonds, suitSizes[(int)Suit.Diamonds]))
+                .Concat(Enumerable.Repeat(Suit.Clubs, suitSizes[(int)Suit.Clubs]))
+                .Concat(Enumerable.Repeat(Suit.Spades, suitSizes[(int)Suit.Spades]))
+                .ToList();
+            Shuffle(cards);
+
+            _markets = new Market[Suits.All.Count];
+            foreach (var suit in Suits.All)
+                _markets[(int)suit] = new Market { Game = this, Suit = suit };
+
+            _players = new Player[players.Length];
+
+            for (int i = 0; i < players.Length; i++)
+            {
+                _players[i] = new Player
+                {
+                    Cash = -buyIn,
+                    Hand = new Hand(cards.Take(handSize)),
+                    ExternalPlayer = players[i],
+                    PlayerGame = new PlayerGame(this, i)
+                };
+                cards.RemoveRange(0, handSize);
+            }
+
+            if (cards.Count != 0)
+                throw new InvalidOperationException("state");
+        }
+
+        public bool Ended
+        {
+            get { return _ended; }
+        }
+
+        public Player[] Players
+        {
+            get { return _players; }
+        }
+
+        private void Shuffle<T>(IList<T> list)
+        {
+            int n = list.Count;
+            while (n > 1)
+            {
+                n--;
+                int k = _random.Next(n + 1);
+                T value = list[k];
+                list[k] = list[n];
+                list[n] = value;
+            }
+        }
+
+        private void End()
+        {
+            if (_ended)
+                return;
+            _ended = true;
+
+            var playersByGoal = _players
+                .Select(player => Tuple.Create(player, player.Hand.Count(_goalSuit)))
+                .OrderByDescending(p => p.Item2)
+                .ToList();
+            int pot = 200;
+
+            foreach (var p in playersByGoal)
+            {
+                int payoff = p.Item2 * 10;
+                pot -= payoff;
+                p.Item1.Cash += payoff;
+            }
+
+            var bestPlayer = playersByGoal.First();
+            var bestPlayers = playersByGoal.TakeWhile(p => p.Item2 == bestPlayer.Item2).ToList();
+            int bonus = pot / bestPlayers.Count;
+
+            foreach (var p in bestPlayers)
+            {
+                int payoff = System.Math.Min(pot, bonus);
+                pot -= payoff;
+                p.Item1.Cash += payoff;
+            }
+
+            if (pot != 0)
+                throw new InvalidOperationException("state");
+        }
+    }
+}
---
+++ Figgie/MainForm.Designer.cs
@@ -0,0 +1,149 @@
+﻿namespace Figgie
+{
+    partial class MainForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.textMessages = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.textHand = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.textCommand = new System.Windows.Forms.TextBox();
+            this.buttonExecute = new System.Windows.Forms.Button();
+            this.labelHeader = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // textMessages
+            // 
+            this.textMessages.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.textMessages.BackColor = System.Drawing.SystemColors.Window;
+            this.textMessages.Font = new System.Drawing.Font("Courier New", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textMessages.Location = new System.Drawing.Point(12, 33);
+            this.textMessages.Multiline = true;
+            this.textMessages.Name = "textMessages";
+            this.textMessages.ReadOnly = true;
+            this.textMessages.Size = new System.Drawing.Size(479, 308);
+            this.textMessages.TabIndex = 0;
+            // 
+            // label1
+            // 
+            this.label1.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 353);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(59, 13);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Your hand:";
+            // 
+            // textHand
+            // 
+            this.textHand.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.textHand.BackColor = System.Drawing.SystemColors.Window;
+            this.textHand.Font = new System.Drawing.Font("Courier New", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textHand.Location = new System.Drawing.Point(77, 347);
+            this.textHand.Name = "textHand";
+            this.textHand.ReadOnly = true;
+            this.textHand.Size = new System.Drawing.Size(414, 26);
+            this.textHand.TabIndex = 2;
+            // 
+            // label2
+            // 
+            this.label2.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(12, 385);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(57, 13);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Command:";
+            // 
+            // textCommand
+            // 
+            this.textCommand.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.textCommand.Font = new System.Drawing.Font("Courier New", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textCommand.Location = new System.Drawing.Point(77, 379);
+            this.textCommand.Name = "textCommand";
+            this.textCommand.Size = new System.Drawing.Size(333, 26);
+            this.textCommand.TabIndex = 4;
+            // 
+            // buttonExecute
+            // 
+            this.buttonExecute.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.buttonExecute.Location = new System.Drawing.Point(416, 380);
+            this.buttonExecute.Name = "buttonExecute";
+            this.buttonExecute.Size = new System.Drawing.Size(75, 23);
+            this.buttonExecute.TabIndex = 5;
+            this.buttonExecute.Text = "Execute";
+            this.buttonExecute.UseVisualStyleBackColor = true;
+            // 
+            // labelHeader
+            // 
+            this.labelHeader.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.labelHeader.Location = new System.Drawing.Point(12, 9);
+            this.labelHeader.Name = "labelHeader";
+            this.labelHeader.Size = new System.Drawing.Size(479, 21);
+            this.labelHeader.TabIndex = 6;
+            this.labelHeader.Text = "time";
+            this.labelHeader.TextAlign = System.Drawing.ContentAlignment.TopRight;
+            // 
+            // MainForm
+            // 
+            this.AcceptButton = this.buttonExecute;
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(503, 415);
+            this.Controls.Add(this.labelHeader);
+            this.Controls.Add(this.buttonExecute);
+            this.Controls.Add(this.textCommand);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.textHand);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textMessages);
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "MainForm";
+            this.Text = "Figgie";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textMessages;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox textHand;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox textCommand;
+        private System.Windows.Forms.Button buttonExecute;
+        private System.Windows.Forms.Label labelHeader;
+    }
+}
